eacf80c072a0d8ccfcf578f3f9fa81a904e18f49
Blair Millet <120157336+blairrrrwho@users.noreply.github.com>
2023-04-17 04:10:39 -0500

    controller routes complete - dashboard, home

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -3,111 +3,132 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
-// Dashboard / dashboard displaying posts created by logged in users ===================================
+// Dashboard / dashboard displaying posts created by logged in users ======================================
 router.get('/', withAuth, async (req, res) => {
-  Post.findAll({
-    where: {
-      // ID from the session
-      user_id: req.session.user_id,
-    },
-    attributes: ['id', 'title', 'created_at', 'post_body'],
-    order: [['date', 'DESC']],
-    include: [
-      {
-        model: Comment,
-        attributes: ['id', 'comment_body', 'post_id', 'user_id', 'created_at'],
-        include: {
+  try {
+    const postData = await Post.findAll({
+      where: {
+        // ID from the session
+        user_id: req.session.user_id,
+      },
+      attributes: ['id', 'title', 'created_at', 'post_body'],
+      order: [['date', 'DESC']],
+      include: [
+        {
+          model: Comment,
+          attributes: [
+            'id',
+            'comment_body',
+            'post_id',
+            'user_id',
+            'created_at',
+          ],
+          include: {
+            model: User,
+            attributes: ['username', 'github'],
+          },
+        },
+        {
           model: User,
           attributes: ['username', 'github'],
         },
-      },
-      {
-        model: User,
-        attributes: ['username', 'github'],
-      },
-    ],
-  })
-    .then((postData) => {
+      ],
+    });
+    then((postData) => {
       // serialize data before passing to template
       const posts = postData.map((post) => post.get({ plain: true }));
       res.render('dashboard', { posts, loggedIn: true });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
     });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 // Editing a post / rendering edit-post page ======================================================
-router.get('/edit/:id', withAuth, (req, res) => {
-  Post.findOne({
-    where: {
-      id: req.params.id,
-    },
-    attributes: ['id', 'post_body', 'title', 'created_at'],
-    include: [
-
-      {
-        model: Comment,
-        attributes: ['id', 'comment_body', 'post_id', 'user_id', 'created_at'],
-        include: {
+router.get('/edit/:id', withAuth, async (req, res) => {
+  try {
+    const editPost = await Post.findByPk({
+      where: {
+        id: req.params.id,
+      },
+      attributes: ['id', 'post_body', 'title', 'created_at'],
+      include: [
+        {
+          model: Comment,
+          attributes: [
+            'id',
+            'comment_body',
+            'post_id',
+            'user_id',
+            'created_at',
+          ],
+          include: {
+            model: User,
+            attributes: ['username', 'github'],
+          },
+        },
+        {
           model: User,
           attributes: ['username', 'github'],
         },
-      },      
-      {
-        model: User,
-        attributes: ['username', 'github'],
-      },
-    ],
-  })
-    .then((postData) => {
-      if (!postData) {
-        res.status(404).json({ message: 'No post found with this ID.' });
-        return;
-      }
-      // serialize the data
-      const post = postData.get({ plain: true });
-      res.render('edit-post', { post, loggedIn: true });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
+      ],
     });
+    if (!editPost) {
+      res.status(404).json({ message: 'No post found with this ID.' });
+      return;
+    }
+    // serialize the data
+    const post = editPost.get({ plain: true });
+
+    res.render('edit-post', {
+      // post vs ...post?????????????????
+      post,
+      loggedIn: true,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 // Creating a post / rendering add-post page ======================================================
-router.get('/create/', withAuth, (req, res) => {
-  Post.findAll({
-    where: {
-      // use the ID from the session
-      user_id: req.session.user_id,
-    },
-    attributes: ['id', 'title', 'created_at', 'post_body'],
-    include: [
-      {
-        model: Comment,
-        attributes: ['id', 'comment_body', 'post_id', 'user_id', 'created_at'],
-        include: {
+router.get('/create/', withAuth, async (req, res) => {
+  try {
+    const newPost = await Post.findAll({
+      where: {
+        // use the ID from the session
+        user_id: req.session.user_id,
+      },
+      attributes: ['id', 'title', 'created_at', 'post_body'],
+      include: [
+        {
+          model: Comment,
+          attributes: [
+            'id',
+            'comment_body',
+            'post_id',
+            'user_id',
+            'created_at',
+          ],
+          include: {
+            model: User,
+            attributes: ['username', 'github'],
+          },
+        },
+        {
           model: User,
           attributes: ['username', 'github'],
         },
-      },
-      {
-        model: User,
-        attributes: ['username', 'github'],
-      },
-    ],
-  })
-    .then((postData) => {
-      // serialize data before passing to template
-      const posts = postData.map((post) => post.get({ plain: true }));
-      res.render('add-post', { posts, loggedIn: true });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
+      ],
     });
+    // serialize data before passing to template
+    const posts = newPost.map((post) => post.get({ plain: true }));
+    res.render('add-post', { posts, loggedIn: true });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -34,23 +34,56 @@ router.get('/', async (req, res) => {
         },
       ],
     });
-    // Serialize data so the template can read it
-    const posts = postData.map((post) => post.get({ plain: true }));
-    res.render('homepage', {
-      posts,
-      loggedIn: req.session.loggedIn,
-    });
+      // serialize data so the template can read it
+      const posts = postData.map((post) => post.get({ plain: true }));
+      res.render('homepage', {
+        posts,
+        loggedIn: req.session.loggedIn,
+      });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
 });
 
+// Login / directs to login page ==========================================================================
+router.get('/login', async (req, res) => {
+  try {
+    res.render('login', {});
+  } catch (err) {
+    // if the user is already logged in, redirect the request to the homepage
+    // take out?
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// SignUp / directs to signup page - Use withAuth middleware to prevent access to route ====================
+router.get('/signup', withAuth, async (req, res) => {
+  try {
+    res.render('signup', {});
+  } catch (err) {
+    // if the user is already logged in, redirect the request to the homepage
+    // take out?
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+
 // Single post view / directs to single-post page =====================================================
 // http://localhost:3001/post/1
 router.get('/post/:id', async (req, res) => {
   try {
-    const specificPost = await Post.findByPk({
+    const postData = await Post.findByPk({
       where: { id: req.params.id },
       attributes: ['id', 'title', 'created_at', 'post_body'],
       include: [
@@ -74,48 +107,20 @@ router.get('/post/:id', async (req, res) => {
         },
       ],
     });
-    // serialize the data
-    const post = specificPost.comments.map((test) => test.get({ plain: true }));
-    // pass data to template
-    res.status(200).render('single-post', {
-      post,
-      loggedIn: req.session.loggedIn,
-    });
+      if (!postData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      // serialize the data
+      const post = postData.comments.map((test) => test.get({ plain: true }));
+      // pass data to template
+      res.status(200).render('single-post', {
+        post,
+        loggedIn: req.session.loggedIn,
+      });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
-  }
-});
-
-// Login / directs to login page ==========================================================================
-router.get('/login', async (req, res) => {
-  try {
-    res.render('login', {});
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-  // if the user is already logged in, redirect the request to the homepage
-  // take out?
-  if (req.session.loggedIn) {
-    res.redirect('/');
-    return;
-  }
-});
-
-// SignUp / directs to signup page - Use withAuth middleware to prevent access to route ====================
-router.get('/signup', withAuth, async (req, res) => {
-  try {
-    res.render('signup', {});
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-  // if the user is already logged in, redirect the request to the homepage
-  // take out?
-  if (req.session.loggedIn) {
-    res.redirect('/');
-    return;
   }
 });
 
